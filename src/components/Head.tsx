2c02989cf52fbb1ea7c02ade1d7245b198782f8f
Daniel-Stafford <stafford.daniel@gmail.com>
2019-12-27 08:45:23 -0800

    basic typescript type examples

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,9 +3,22 @@ import React from "react"
 //Title
 //isActive
 
+type User = {
+  name: string
+}
+
 type Props = {
   title: string //required
   isActive?: boolean //optional
+  thing?: number
+  thing2?: string[] //array of strings
+  status?: "loading" | "loaded" //union type
+  thing3?: {} //empty object
+  thing4?: {
+    name: string
+  }
+  func?: () => void
+  user?: User
 }
 
 export const Head = ({ title, isActive = true }: Props) => {
