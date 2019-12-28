1f9f5892e57e2a77258032052434fd10a789fcd4
Daniel-Stafford <stafford.daniel@gmail.com>
2019-12-27 18:15:51 -0800

    optional chaining

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,11 +5,11 @@ export const Input = () => {
   // const [name, setName] = useState<string>("") //explicit type as string
   // const [name, setName] = useState<null | string>(null) // explicit type as string or null
   console.log("name is:", name)
-  // const ref = useRef<HTMLInputElement>(null!) //null! exclamation makes read-only, most typical
-  const ref = useRef<HTMLInputElement | null>(null) // mutable
-  if (ref && ref.current) {
-    console.log("ref", ref.current.value)
-  }
+  const ref = useRef<HTMLInputElement>(null!) //null! exclamation makes read-only, most typical
+  // const ref = useRef<HTMLInputElement | null>(null) // mutable
+
+  console.log("ref", ref?.current?.value) //optional chaining to check if variable exists
+
   return (
     <input ref={ref} value={name} onChange={e => setName(e.target.value)} />
   )
