a9f57319982e4a1b777866cb21b69325e4ff0d76
Daniel-Stafford <stafford.daniel@gmail.com>
2019-12-27 18:04:00 -0800

    useRef

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,16 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 
 export const Input = () => {
   const [name, setName] = useState("") //inferred type as string
+  // const [name, setName] = useState<string>("") //explicit type as string
+  // const [name, setName] = useState<null | string>(null) // explicit type as string or null
   console.log("name is:", name)
-  return <input value={name} onChange={e => setName(e.target.value)} />
+  // const ref = useRef<HTMLInputElement>(null!) //null! exclamation makes read-only, most typical
+  const ref = useRef<HTMLInputElement | null>(null) // mutable
+  if (ref && ref.current) {
+    console.log("ref", ref.current.value)
+  }
+  return (
+    <input ref={ref} value={name} onChange={e => setName(e.target.value)} />
+  )
 }
